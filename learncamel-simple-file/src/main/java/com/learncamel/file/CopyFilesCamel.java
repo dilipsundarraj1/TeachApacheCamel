abf1bade2b49b603573247cbf73367cc0b53e29b
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2017-06-05 21:28:06 -0500

    added the simple apache camel example

---
+++ learncamel-simple-file/src/main/java/com/learncamel/file/CopyFilesCamel.java
@@ -0,0 +1,33 @@
+package com.learncamel.file;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * Created by z001qgd on 5/30/17.
+ */
+public class CopyFilesCamel {
+
+    public static void main(String[] args) {
+        CamelContext context= new DefaultCamelContext();
+
+        try{
+            context.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("file:data/input?noop=true")
+                            .to("file:data/output");
+                }
+            });
+
+            context.start();
+            Thread.sleep(5000);
+            context.stop();
+
+        }catch(Exception e){
+            System.out.println("Inside Exception : " + e);
+        }
+
+    }
+}
---
+++ learncamel-simple-file/src/main/java/com/learncamel/file/CopyFilesWithoutCamel.java
@@ -0,0 +1,44 @@
+package com.learncamel.file;
+
+import java.io.*;
+
+/**
+ * Created by z001qgd on 4/14/17.
+ */
+public class CopyFilesWithoutCamel {
+
+    public static void main(String[] args) throws IOException {
+
+        //Step 1 - Create a file Object for the the directories to read and write.
+        File inputDirectory = new File("data/input");
+        File outputDirectory = new File("data/output");
+
+        outputDirectory.mkdir();
+
+        //Step 2 -  Read the files and Iterate the files
+        File[] files = inputDirectory.listFiles();
+
+        for (File source : files) {
+            if (source.isFile()) {
+                File dest = new File(
+                        outputDirectory.getPath()
+                                + File.separator
+                                + source.getName());
+        //Step 3 : Create a Output Stream to write the files.
+
+                OutputStream oStream = new FileOutputStream(dest);
+                byte[] buffer = new byte[(int) source.length()];
+                FileInputStream iStream = new FileInputStream(source);
+                iStream.read(buffer);
+                try {
+                    oStream.write(buffer);
+                } finally {
+                    oStream.close();
+                    iStream.close();
+                }
+            }
+        }
+
+    }
+
+}
