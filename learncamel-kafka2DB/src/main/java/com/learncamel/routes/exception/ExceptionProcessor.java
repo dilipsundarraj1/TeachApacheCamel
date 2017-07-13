36402d027e61b6cb94d8caedae108491b9937469
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2017-07-13 06:09:23 -0500

    added the code

---
+++ learncamel-kafka2DB/src/main/java/com/learncamel/routes/exception/ExceptionProcessor.java
@@ -0,0 +1,21 @@
+package com.learncamel.routes.exception;
+
+import org.apache.camel.Exchange;
+
+/**
+ * Created by z001qgd on 7/12/17.
+ */
+public class ExceptionProcessor implements org.apache.camel.Processor {
+    public void process(Exchange exchange) throws Exception {
+
+        Exception e = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
+        System.out.println("Acutal Exceptipn Message "  + e.getMessage());
+        System.out.println("Acutal Exceptipn Class "  + e.getClass());
+
+        String failedEndoint = (String) exchange.getProperty(Exchange.FAILURE_ENDPOINT);
+        System.out.println("Failed Endpoint : " + failedEndoint);
+
+
+        exchange.getIn().setBody("Exception happened in the route.");
+    }
+}
--- learncamel-kafka2DB/src/main/java/com/learncamel/routes/kafka2jdbc/Kafka2JdbcRoute.java
+++ learncamel-kafka2DB/src/main/java/com/learncamel/routes/kafka2jdbc/Kafka2JdbcRoute.java
@@ -1,18 +1,23 @@
 package com.learncamel.routes.kafka2jdbc;
 
+import com.learncamel.routes.exception.ExceptionProcessor;
 import com.learncamel.routes.jdbc.InsertProcessor;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.kafka.common.KafkaException;
+import org.postgresql.util.PSQLException;
 
 /**
  * Created by z001qgd on 7/11/17.
  */
 public class Kafka2JdbcRoute extends RouteBuilder {
 
-
     public void configure() throws Exception {
 
+        onException(PSQLException.class, Exception.class).handled(true).log("Exception While inserting messages.").process(new ExceptionProcessor());
+
         from("kafka:localhost:9092?topic=my-first-topic&groupId=group1&consumersCount=1&autoOffsetReset=latest")
-                .log("${body}")
+                .to("log:?level=INFO&showBody=true")
                 .process(new InsertProcessor())
                 .to("jdbc:myDataSource")
                 .to("sql:select * from messages?dataSource=myDataSource")
