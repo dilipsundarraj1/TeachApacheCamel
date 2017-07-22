5d64f334eeed90a91c0a5d3c2c4831279c9ee4b8
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2017-07-22 11:32:29 -0500

    added the wiretap

---
+++ learn-camel-eips/src/main/java/com/learncamel/eip/wiretap/WireTapRoute.java
@@ -0,0 +1,15 @@
+package com.learncamel.eip.wiretap;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Created by z001qgd on 7/22/17.
+ */
+public class WireTapRoute extends RouteBuilder {
+
+    public void configure() throws Exception {
+        from("file:input?noop=true")
+                .wireTap("file:debug")
+                .to("file:all");
+    }
+}
---
+++ learn-camel-eips/src/test/java/com/learncamel/eip/wiretap/WireTapRouteTest.java
@@ -0,0 +1,33 @@
+package com.learncamel.eip.wiretap;
+
+import com.learncamel.eip.multicast.MultiCastRoute;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * Created by z001qgd on 7/22/17.
+ */
+public class WireTapRouteTest extends CamelTestSupport {
+
+    @Override
+    public RouteBuilder createRouteBuilder(){
+
+        return new WireTapRoute();
+    }
+
+    @Test
+    public void wireTapRoute() throws InterruptedException {
+
+        Thread.sleep(5000);
+        File file = new File("all");
+
+        assertTrue(file.isDirectory());
+
+        File fileDebug = new File("debug");
+
+        assertTrue(fileDebug.isDirectory());
+    }
+}
