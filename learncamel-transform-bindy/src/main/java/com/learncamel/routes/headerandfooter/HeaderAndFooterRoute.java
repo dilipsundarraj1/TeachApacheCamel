9159926b3fc171d4f7b91af97bbb326590e5521d
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2018-09-28 06:01:18 -0500

    added the route class

---
+++ learncamel-transform-bindy/src/main/java/com/learncamel/routes/headerandfooter/HeaderAndFooterRoute.java
@@ -0,0 +1,33 @@
+package com.learncamel.routes.headerandfooter;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+public class HeaderAndFooterRoute extends RouteBuilder  {
+
+
+    public void configure() throws Exception {
+
+        from("file:data/headerandfooter/input?fileName=samplefile&noop=true")
+                .split((body().tokenize("(MH7000)")))
+                .streaming()
+                .process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        String body = (String) exchange.getIn().getBody();
+                        if(((String) exchange.getIn().getBody()).contains("(") || ((String) exchange.getIn().getBody()).contains(")")){
+
+                            body =  body.replace("(","");
+                            body =  body.replace(")","");
+                            body =  body.replace("RRRR","");
+
+                        };
+                        exchange.getIn().setBody(body);
+                    }
+                })
+               .log("Tokenized output is : ${body}")
+                .choice()
+                .when(body().isNotEqualTo(""))
+                .to("file:data/headerandfooter/output?fileName=${file:name}-${date:now:yyyyMMddHHmmssSSS}");
+    }
+}
